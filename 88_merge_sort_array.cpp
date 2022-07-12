d26bdefcbd6f1c164b34987b5994a755d6c9b1c9
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-07-12 21:01:27 +0200

    Update 88_merge_sort_array.cpp

--- 88_merge_sort_array.cpp
+++ 88_merge_sort_array.cpp
@@ -1,1 +1,24 @@
 
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+         
+       vector<int>z;
+        for(int i = 0; i < m; i++)
+            {
+                    z.push_back(nums1[i]);
+            }
+        
+        for(int i = 0; i < n; i++)
+            {
+                    z.push_back(nums2[i]);
+            }
+        
+        sort(z.begin(), z.end());
+        
+        for(int i = 0; i<nums1.size(); i++)
+        {
+            nums1[i] = z[i];
+        }
+    }
+};
