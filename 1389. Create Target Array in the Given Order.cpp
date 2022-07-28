932c025c7bf5bdc479b57a951c9ee2ad9cada49b
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-07-29 01:11:04 +0200

    Update 1389. Create Target Array in the Given Order.cpp

--- 1389. Create Target Array in the Given Order.cpp
+++ 1389. Create Target Array in the Given Order.cpp
@@ -1,1 +1,13 @@
 
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) 
+    {
+                vector<int> poldon;
+                for(int i = 0; i<nums.size(); i++)
+                {
+                    poldon.emplace(poldon.begin()+index[i],nums[i]);
+                }
+            return poldon;
+    }
+};
