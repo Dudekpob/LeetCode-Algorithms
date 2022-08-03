89a6568ec9f2f50e39a1456d51e54638d9fa3d57
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-08-03 16:13:05 +0200

    Update 2169. Count Operations to Obtain Zero.cpp

--- 2169. Count Operations to Obtain Zero.cpp
+++ 2169. Count Operations to Obtain Zero.cpp
@@ -1,1 +1,34 @@
 
+class Solution {
+public:
+    int countOperations(int num1, int num2) 
+    {
+          int num = num1>num2 ? num1 : num2;
+          int cnt = 0;
+            if(num1 == 0 || num2 == 0)
+            {
+                return 0;
+            }
+        
+         for(int i = 0; i < num; i++)
+         {
+             if(num1>num2 )
+             {
+                 num1 = num1 - num2;
+                 cnt++;
+             }
+             if(num2>num1)
+             {
+                 num2 = num2 - num1;
+                 cnt++;
+             }
+             if(num2 == num1)
+             {
+                 num2 = num2 - num1;
+                 cnt++;
+                 return  cnt;
+             }
+         }
+        return cnt;
+    }
+};
