b1c34d036c295f63a9c6072dcb3f4dd24e6d2c02
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-08-08 14:21:26 +0200

    Update 590. N-ary Tree Postorder Traversal.cpp

--- 590. N-ary Tree Postorder Traversal.cpp
+++ 590. N-ary Tree Postorder Traversal.cpp
@@ -1,1 +1,43 @@
 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+ 
+ void traversal(Node* root,vector<int>& vec)
+    {
+        if(!root)
+            return;
+        for(Node* i:root->children)
+        {
+            traversal(i,vec);
+        }
+        vec.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) 
+    {
+      vector<int> vec;
+      traversal(root,vec);
+        return vec;
+     
+    }
+    
+};
