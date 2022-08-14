f3f66bd7e3da5ac25744eebd3fe1f0cf50754ee3
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-08-14 17:48:44 +0200

    Update 504. Base 7.cpp

--- 504. Base 7.cpp
+++ 504. Base 7.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+    string convertToBase7(int num) {
+
+         int ret =0;
+        int j = 1;
+        while(num)
+        { 
+            ret += (num%7)*j;
+            num /=7;
+            j*=10;
+        }
+        
+        return to_string(ret);
+        
+    }
+};
