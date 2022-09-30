f69c2eec8a8dd25b35b17265f95931744c99b360
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-10-01 00:07:47 +0200

    Update 1137. N-th Tribonacci Number.cpp
    Tribbonaci 100 % faster Dynamic Programming

--- 1137. N-th Tribonacci Number.cpp
+++ 1137. N-th Tribonacci Number.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+public:
+    int climbStairs(int n) 
+    { 
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        if(n == 3)
+            return 3;
+        int cnt_one = 1;
+        int cnt = 2;
+        int counter = 0;
+       vector<int>z;
+              z.push_back(0);
+        z.push_back(1);
+         z.push_back(2);
+          z.push_back(3);
+        if(n>3)
+        {
+            for(int i = 3; i<n; i++)
+            {
+                
+                counter = z[i-1]+z[i];
+                z.push_back(counter);
+            }
+        }
+        return counter;
+    }
+};
