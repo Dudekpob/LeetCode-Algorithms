07d05fd9df0892c2afd8ad1aa3a95206d2da7b54
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-10-16 19:47:03 +0200

    Update 122. Best Time to Buy and Sell Stock II.cpp
    need to repair this code

--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -1,1 +1,29 @@
 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int sum = 0;
+        int max = 0;
+        for(int i = 0; i<prices.size(); i++)
+        {
+           for(int j = 0; j<prices.size(); j++)
+           {
+                if(i!=j && i > j && prices[j+1] > prices[j])
+                {
+                    if(max < prices[j+1] - prices[j])
+                    {
+                         max = prices[j+1] - prices[j];
+                        cout<<max<<endl;
+                    }
+                    
+                }
+               else if(i!=j && i > j && prices[j+1] < prices[j])
+               {
+                   sum +=max;
+                   max =0;
+               }
+           }
+        }
+        return sum;
+    }
+};
