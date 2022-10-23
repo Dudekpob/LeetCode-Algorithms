80af07bbb760eed66a5ee992162614ee672d2875
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-10-23 08:30:21 +0200

    Create 220. Contains Duplicate III.cpp
    need to fix it!!!

---
+++ 220. Contains Duplicate III.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t)
+    {
+                         unordered_map<int> k;
+                       for(int i = 0; i<nums.size(); i++)
+                       {
+                         i      
+                       }
+    }
+};
