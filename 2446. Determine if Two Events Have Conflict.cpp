8e464b5484be931622ee212263f28ce0a02c7ece
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-10-26 20:48:30 +0200

    Create 2446. Determine if Two Events Have Conflict.cpp
    need to fix it!! Wrong code

---
+++ 2446. Determine if Two Events Have Conflict.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2) 
+    {
+           int hour1 = stoi((event1[0]-48)+(event1[1]-48));
+      
+           int min1 = stoi((event1[3]-48)+(event1[4]-48));
+        
+
+           int hour2 = stoi((event2[0]-48)+(event2[1]-48));
+      
+           int min2 = stoi((event2[3]-48)+(event2[4]-48));
+        
+            int s_houre1 = hour2-hour1;
+        
+        
+        if(hour1 > )
+          
+    }
+};
