62634b65909d7efb839eb8178d819a03d203d5ad
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-10-27 20:59:52 +0200

    Update 2446. Determine if Two Events Have Conflict.cpp
    need to fix it!!

--- 2446. Determine if Two Events Have Conflict.cpp
+++ 2446. Determine if Two Events Have Conflict.cpp
@@ -6,12 +6,25 @@ public:
       
            int min1 = stoi((event1[3]-48)+(event1[4]-48));
         
-
            int hour2 = stoi((event2[0]-48)+(event2[1]-48));
       
            int min2 = stoi((event2[3]-48)+(event2[4]-48));
         
+           int hour11 = stoi((event1[5]-48)+(event1[6]-48));
+      
+           int min11 = stoi((event1[8]-48)+(event1[9]-48));
+        
+           int hour21 = stoi((event2[5]-48)+(event2[6]-48));
+      
+           int min21 = stoi((event2[8]-48)+(event2[9]-48));
+        
            int s_houre1 = hour2-hour1;
+            
+        
+           if(hour1 >= hour2 && min1 < min2 && hour11 )
+           {
+               
+           }
         
         
         if(hour1 > )
