838ce5fb76a410b8d67706d4d3ccccf308c58f07
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-11-05 20:25:31 +0100

    Create 415. Add Strings.cpp
    Need to fix this code !! Doesn't work

---
+++ 415. Add Strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) 
+    {
+  
+        int add = 0;
+        string sum;
+        
+        int size = num1.size() > num2.size() ? num1.size() : num2.size();
+        
+        for(int i = 0; num1.size(); i++)
+        {
+            
+        }
+        
+        return "123";
+    }
+};
