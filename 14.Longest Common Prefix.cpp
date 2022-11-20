3f7872826ee31f33330d38282777d2edc0973d54
EEpoldon <36985894+Dudekpob@users.noreply.github.com>
2022-11-20 15:22:37 +0100

    Update 14.Longest Common Prefix.cpp

--- 14.Longest Common Prefix.cpp
+++ 14.Longest Common Prefix.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) 
+    {
+        int n = strs.size();
+        if(n == 0)
+            return "";
+        
+        string ret = "";
+        
+        sort(strs.begin(), strs.end());
+        
+        string word_start = strs[0];
+        string word_end = strs[n-1];
+        
+        for (int i = 0; i < word_start.size(); i++){
+            if (word_start[i] == word_end[i])
+                ret = ret + word_start[i];
+            else
+                break;
+        }
+        return ret;
+    }
+};
